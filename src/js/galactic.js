e6736945da25ec8315aa2139e5e7396e872cb994
Ebru Rice <ebruerenb@gmail.com>
2022-01-03 16:56:07 -0800

    Galactic constructor test green phase

---
+++ src/js/galactic.js
@@ -0,0 +1,8 @@
+export default class Galactic {
+  constructor(age, smoking, health, exercise) {
+    this.age = age;
+    this.smoking = smoking;
+    this.health = health;
+    this.exercise = exercise;
+  }
+}  
