a786b315bcb7339429bf75e23145ad71226fa141
Ebru Rice <ebruerenb@gmail.com>
2022-01-03 17:16:11 -0800

    marsAge test red phase

--- _test_/galactic.test.js
+++ _test_/galactic.test.js
@@ -1,11 +1,17 @@
 import Galactic from "../src/js/galactic.js"
 
 describe('Galactic', () => {
+  let galactic;
+  beforeEach(() =>{
+    galactic = new Galactic(30,2,2,2);
+  });
  test('should correctly create an object with four properties', () => {
-    const galactic = new Galactic(30,2,2,2);
     expect(galactic.age).toEqual(30);
     expect(galactic.smoking).toEqual(2);
     expect(galactic.health).toEqual(2);
     expect(galactic.exercise).toEqual(2);
+  });
+  test('should correctly determine age in Mars', () => {
+    expect(galactic.marsAge()).toEqual(15.9);
   }); 
 });
