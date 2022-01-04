1bd6241fdf10f3ea7dd2b49fe556e8b756eb801a
Ebru Rice <ebruerenb@gmail.com>
2022-01-03 20:18:15 -0800

    yearsToLive test green phase

--- src/js/galactic.js
+++ src/js/galactic.js
@@ -17,4 +17,14 @@ export default class Galactic {
   jupiterAge() {
     return Math.ceil(this.age / 11.86);
   }
+  yearsToLive(avAge) {
+    let healthPoint = this.smoking + this.health + this.exercise;
+    if (healthPoint >= 5) {
+      return Math.ceil(avAge/4 + avAge - this.age);
+    } else if (healthPoint === 3 || healthPoint === 4) {
+      return Math.ceil(avAge/8 + avAge - this.age);
+    } else {
+      return Math.ceil(avAge - avAge/10 - this.age);
+    }  
+  } 
 }  
