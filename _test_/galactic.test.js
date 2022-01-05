0431672a6475711f2da0f539359850ff5877dc80
Ebru Rice <ebruerenb@gmail.com>
2022-01-04 19:35:23 -0800

    red phase for refactored code planetAges function

--- _test_/galactic.test.js
+++ _test_/galactic.test.js
@@ -11,19 +11,7 @@ describe('Galactic', () => {
     expect(galactic.health).toEqual(2);
     expect(galactic.exercise).toEqual(2);
   });
-  test('should correctly determine age in Mars', () => {
-    expect(galactic.marsAge()).toEqual(16);
-  }); 
-  test('should correctly determine age in Mercury', () => {
-    expect(galactic.mercuryAge()).toEqual(125);
-  }); 
-  test('should correctly determine age in Venus', () => {
-    expect(galactic.venusAge()).toEqual(49);
-  }); 
-  test('should correctly determine age in Jupiter', () => {
-    expect(galactic.jupiterAge()).toEqual(3);
-  }); 
-  test('should correctly determine how many years a user has left to live', () => {
-    expect(galactic.yearsToLive(72, 30)).toEqual(60);
+  test('should correctly determine ages in planets Earth, Jupiter, Venus, Mercury, Mars', () => {
+    expect(galactic.planetAges()).toEqual(30, 16, 125, 49, 3);
   }); 
 });
