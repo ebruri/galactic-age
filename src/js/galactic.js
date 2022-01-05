126374dd071c514b5e2e46fbcad64d4e5e84ae95
Ebru Rice <ebruerenb@gmail.com>
2022-01-04 19:40:54 -0800

    green phase for refactored yearsToLive function

--- src/js/galactic.js
+++ src/js/galactic.js
@@ -10,4 +10,21 @@ export default class Galactic {
     ages.push(Math.ceil(this.age), Math.ceil(this.age / 1.88), Math.ceil(this.age / 0.24), Math.ceil(this.age / 0.62), Math.ceil(this.age / 11.86));
     return ages; 
   }
+  yearsToLive() {
+    let average =[72, 39, 300, 117, 7];
+    let healthPoint = this.smoking + this.health + this.exercise;
+    let ages = this.planetAges();
+    console.log(ages);
+    let yearsLeft = [];
+    for (let i = 0; i < average.length; i++) {
+        if (healthPoint >= 5) {
+          yearsLeft.push(Math.ceil(average[i]/4 + average[i] - ages[i]));
+        } else if (healthPoint === 3 || healthPoint === 4) {
+          yearsLeft.push(Math.ceil(average[i]/8 + average[i] - ages[i]));
+        } else {
+          yearsLeft.push(Math.ceil(average[i] - average[i]/10 - ages[i]));
+        }
+    } 
+    return yearsLeft;
+ }
 }  
